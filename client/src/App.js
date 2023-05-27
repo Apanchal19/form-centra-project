4cecf70fa75ec36a782b34c0fc193c189c0232a0
Akshay Panchal <akshay.panchalhp@gmail.com>
2023-05-27 13:31:14 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CustomerForm from './components/CustomerForm';
+
+function App() {
+  return (
+    <div >
+      <CustomerForm />
+    </div>
+    
+  );
+}
+
+export default App;
